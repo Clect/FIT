23bba957b56ef4f35c9c681dce6d440247a44326
Clect <clect@foxmail.com>
2019-02-13 09:45:11 +0800

    pow 更好的算法

--- leetcode/l231.js
+++ leetcode/l231.js
@@ -3,6 +3,15 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
+    if(n > 2048) {
+        return isPowerOfTwoBig(n);
+    }
+    else {
+        return isPowerOfTwoSmall(n);
+    }
+}
+
+var isPowerOfTwoSmall = function(n) {
     let tar = n;
     if(n <= 0) {
         return false;
@@ -13,6 +22,29 @@ var isPowerOfTwo = function(n) {
             return false;
         }
     }
-
     return true;
 };
+
+let P10 = 1024;
+var isPowerOfTwoBig = function(n) {
+    let tar = n;
+    if(n <= 0) {
+        return false;
+    }
+
+    while(tar > P10) {
+        tar /= P10;
+        if(tar % 1 != 0) {
+            return false;
+        }
+    }
+
+    if(tar === 1 || tar === 2 || tar === 4 || 
+        tar === 8 || tar === 16 || tar === 32 || 
+        tar === 64 || tar === 128 || tar === 256 || 
+        tar === 512 || tar === 1024) {
+        return true;
+    }
+
+    return false;
+}
---
+++ leetcode/l59.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    let ret = [];
+    // ret[n / 2 - 1][n / 2]
+    for(let i = 0; i < n;i++) {
+        for(let j = 0;j < n;j++) {
+
+        }
+    }
+    ret.push()
+    
+    return ret;
+};
